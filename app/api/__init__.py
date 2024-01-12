50e1d5cef374d75a2f6d78922b2761eb9acb5c0b
yukitake <koshi.yukitake@gmail.com>
2024-01-12 09:28:59 +0900

    Routingの前にCookieヘッダを検証する

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,3 +1,6 @@
+import json
+import time
+
 from fastapi import (
     APIRouter,
     Depends,
@@ -5,7 +8,7 @@ from fastapi import (
     Security,
     status,
 )
-from fastapi.security import APIKeyHeader
+from fastapi.security import APIKeyHeader, APIKeyCookie
 from .wallets.views import router as wallets_router
 from .admin.users.views import router as admin_router
 from .auth.views import router as auth_router
@@ -25,8 +28,28 @@ async def get_api_key(
 
     return api_key_header
 
+async def get_access_token(
+        cookie: str = Security(
+            APIKeyCookie(
+                name="session",
+                # Cookieの値がない場合はエラーにする
+                auto_error=True,
+            )
+        ),
+) -> str:
+    # TODO: CognitoTokenの検証
+    decoded_cookie = json.loads(cookie)
+    # トークンの期限が切れている場合はリフレッシュトークンを使ってトークンを更新する
+    if decoded_cookie["exp"] < time.time():
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED
+        )
+    
+    return cookie
+
 router = APIRouter(
     dependencies=[Depends(get_api_key)]
+    # dependencies=[Depends(get_access_token)]
 )
 
 router.include_router(admin_router)
