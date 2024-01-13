6b449a52963138703405ea11b832ad23471e58be
yukitake <koshi.yukitake@gmail.com>
2024-01-13 09:29:20 +0900

    現在時刻を環境に応じてコントロールできるようにする

--- app/dependencies/datetime.py
+++ app/dependencies/datetime.py
@@ -1,6 +1,15 @@
 from typing import Annotated
 from datetime import datetime
-from pydantic import WrapSerializer
-from utils.datetime import to_utc
 
-UTCDatetime = Annotated[datetime, WrapSerializer(to_utc)]
+from pydantic import WrapSerializer
+
+from utils.datetime import to_utc
+from config import Settings
+
+settings = Settings()
+
+if settings.status == "testing":
+    # テスト用の特定の日時を返すdatetimeを定義することで、現在時刻等の変動する値を固定できる
+    UTCDatetime = Annotated[datetime, WrapSerializer(to_utc)]
+else:
+    UTCDatetime = Annotated[datetime, WrapSerializer(to_utc)]
